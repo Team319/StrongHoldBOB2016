53759bf17de1f83b19bd85893e9f0db4730f4afc
Team319 <BigBadBob319@gmail.com>
2016-01-29 21:27:39 -0500

    missed one

---
+++ src/org/usfirst/frc319/commands/CollectAndStop.java
@@ -0,0 +1,37 @@
+// Strong Hold BOB 2016 Commands (Collector In)
+
+package org.usfirst.frc319.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc319.Robot;
+
+/**
+ *
+ */
+public class CollectAndStop extends Command {
+
+    public CollectAndStop() {
+
+        requires(Robot.collector);
+
+    }
+
+    protected void initialize() {
+    }
+
+    protected void execute() {
+    	
+    
+    }
+
+    protected boolean isFinished() {
+        return Robot.collector.getBoulderSensor();
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+    
+}
